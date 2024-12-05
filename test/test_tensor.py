e62de438e73b5ee0d152de2dbadb089de86d11f1
manu12121999 <manu12121999@gmail.com>
2024-12-05 16:30:30 +0100

    Improved unittests

--- test/test_tensor.py
+++ test/test_tensor.py
@@ -1,11 +1,28 @@
 import unittest
 import numpy as np
 import torch
+import ccm
 
-shapes = [
+
+small_shapes = [
     (2,),
     (3,),
-    (2, 2),
+    (2, 1),
+    (1, 2),
+    (2, 3),
+    (3, 2),
+    (3, 3),
+]
+
+small_shapes_non_singleton = [
+    (2,),
+    (3,),
+    (2, 3),
+    (3, 2),
+    (3, 3),
+]
+
+big_shapes_non_singleton = [
     (2, 3),
     (3, 2),
     (3, 3),
@@ -17,6 +34,58 @@ shapes = [
     (2, 2, 2),
     (2, 2, 4),
     (2, 2, 4),
+    (2, 3, 2, 4),
+    (3, 3, 4, 2),
+    (4, 2, 3, 2, 4),
+    (4, 2, 3, 2, 2),
+    (3, 2, 3, 2, 2),
+]
+
+big_shapes = [
+    (2, 3),
+    (3, 2),
+    (3, 3),
+    (1, 3),
+    (3, 1),
+    (3, 3, 3),
+    (3, 2, 3),
+    (3, 2, 4),
+    (3, 4, 2),
+    (2, 4, 2),
+    (2, 2, 2),
+    (2, 2, 4),
+    (2, 2, 4),
+    (3, 3, 3, 1),
+    (1, 2, 2, 4),
+    (1, 2, 2, 4),
+    (2, 3, 2, 4),
+    (2, 3, 1, 4),
+    (3, 3, 4, 2),
+    (4, 3, 2, 1),
+    (2, 3, 2, 1),
+    (4, 2, 3, 2, 1),
+    (4, 2, 3, 1, 2),
+    (3, 2, 3, 1, 2),
+    (2, 1, 3, 2, 4),
+]
+
+all_shapes = [
+    (2,),
+    (3,),
+    (2, 1),
+    (1, 2),
+    (2, 3),
+    (3, 2),
+    (3, 3),
+    (3, 3, 3),
+    (3, 2, 3),
+    (3, 2, 4),
+    (3, 4, 2),
+    (2, 4, 2),
+    (2, 2, 2),
+    (2, 2, 4),
+    (2, 2, 4),
+    (3, 3, 3, 1),
     (1, 2, 2, 4),
     (1, 2, 2, 4),
     (2, 3, 2, 4),
@@ -29,32 +98,104 @@ shapes = [
 ]
 
 
-class TestMatmul(unittest.TestCase):
+class TestTensorOps(unittest.TestCase):
 
-    def generic_various_shapes_test(self, func1, func2):
+    def generic_various_shapes_test(self, func1, func2, init2, shapes):
         # Testing various tensor shapes
         for i in range(len(shapes)):
             for j in range(len(shapes)):
-                a_np = np.random.randint(shapes[i])
-                b_np = np.random.randint(shapes[j])
-                a_torch, b_torch = torch.tensor(a_np), torch.tensor(b_np)
+                mat1 = np.random.randint(0, 10, shapes[i])
+                mat2 = np.random.randint(0, 10, shapes[j])
 
+                mat1_B, mat2_B = init2(mat1), init2(mat2)
+                print("MAT A", mat1_B)
+                print("MAT B", mat2_B)
                 try:
-                    result_np = func1(a_np, b_np)
-                except ValueError:
-                    result_np = np.array([])
+                    result_A = func1(mat1, mat2).tolist()
+                    print(f"RES1", result_A)
+                except (ValueError, AssertionError, RuntimeError):
+                    print("No RES1")
+                    result_A = None
                 try:
-                    result_torch = func2(a_torch, b_torch)
-                except RuntimeError:
-                    result_torch = torch.Tensor([])
+                    result_B = func2(mat1_B, mat2_B).tolist()
+                    print("RES2", result_B)
+                except (ValueError, AssertionError, RuntimeError):
+                    print("No RES2")
+                    result_B = None
 
-                result_np_list = result_np.tolist()
-                result_torch_list = result_torch.tolist()
+                self.assertEqual(result_A, result_B, f"Tensor operation {func2} failed for shapes {mat1.shape} and {mat2.shape}. Elemes {mat1} and {mat2}")
 
-                self.assertEqual(result_np_list, result_torch_list, f"Tensor operation {func2} failed for shapes {a_np.shape} and {b_np.shape}.")
+    @unittest.skip
+    def test_matmul_various_shaped_sanity(self):
+        self.generic_various_shapes_test(func1=np.matmul, func2=torch.matmul, init2=lambda x: torch.Tensor(x).to(torch.int32))
 
-    def test_matmul_various_shapes(self):
-        self.generic_various_shapes_test(np.matmul, torch.matmul)
+    @unittest.skip
+    def test_add_various_shaped_sanity(self):
+        self.generic_various_shapes_test(np.add, torch.add, init2=lambda x: torch.Tensor(x).to(torch.int32))
 
-    def test_add_various_shapes(self):
-        self.generic_various_shapes_test(np.add, torch.add)
+    def test_basic_add(self):
+        mat1 = np.random.randint(0, 10, size=(400, 200))
+        mat2 = np.random.randint(0, 10, size=(400, 200))
+        result_A = mat1 + mat2
+        result_B = ccm.Tensor(mat1.tolist()) + ccm.Tensor(mat2.tolist())
+        self.assertEqual(result_A.tolist(), result_B.tolist(),f"Tensor operation basic add failed.")
+
+    def test_basic_mul(self):
+        mat1 = np.random.randint(0, 10, size=(400, 200))
+        mat2 = np.random.randint(0, 10, size=(400, 200))
+        result_A = mat1 * mat2
+        result_B = ccm.Tensor(mat1.tolist()) * ccm.Tensor(mat2.tolist())
+        self.assertEqual(result_A.tolist(), result_B.tolist(),f"Tensor operation basic mul failed.")
+
+    def test_basic_matmul(self):
+        mat1 = np.random.randint(0, 10, size=(400, 200))
+        mat2 = np.random.randint(0, 10, size=(200, 300))
+        result_A = mat1 @ mat2
+        result_B = ccm.Tensor(mat1.tolist()) @ ccm.Tensor(mat2.tolist())
+        self.assertEqual(result_A.tolist(), result_B.tolist(),f"Tensor operation basic matmul failed.")
+
+    def test_matmul_broadcast(self):
+        mat1 = np.random.randint(0, 10, size=(30, 40, 200))
+        mat2 = np.random.randint(0, 10, size=(200, 30))
+        result_A = mat1 @ mat2
+        result_B = ccm.Tensor(mat1.tolist()) @ ccm.Tensor(mat2.tolist())
+        self.assertEqual(result_A.tolist(), result_B.tolist(),f"Tensor operation broadcast matmul failed.")
+
+    def test_matmul_batched(self):
+        mat1 = np.random.randint(0, 10, size=(4, 40, 200))
+        mat2 = np.random.randint(0, 10, size=(4, 200, 30))
+        result_A = mat1 @ mat2
+        result_B = ccm.Tensor(mat1.tolist()) @ ccm.Tensor(mat2.tolist())
+        self.assertEqual(result_A.tolist(), result_B.tolist(),f"Tensor operation broadcast matmul failed.")
+
+    def test_matmul_various_shaped_int(self):
+        self.generic_various_shapes_test(np.matmul, ccm.Tensor.__matmul__, init2=lambda x: ccm.Tensor(x.tolist()), shapes=all_shapes)
+
+    def test_add_various_shaped_int(self):
+        self.generic_various_shapes_test(np.add, ccm.Tensor.__add__, init2=lambda x: ccm.Tensor(x.tolist()), shapes=all_shapes)
+
+    def test_matmul_small_shaped_int(self):
+        self.generic_various_shapes_test(np.matmul, ccm.Tensor.__matmul__, init2=lambda x: ccm.Tensor(x.tolist()), shapes=small_shapes)
+
+    def test_add_small_shaped_int(self):
+        self.generic_various_shapes_test(np.add, ccm.Tensor.__add__, init2=lambda x: ccm.Tensor(x.tolist()), shapes=small_shapes)
+
+    def test_matmul_small_non_sing_shaped_int(self):
+        self.generic_various_shapes_test(np.matmul, ccm.Tensor.__matmul__, init2=lambda x: ccm.Tensor(x.tolist()), shapes=small_shapes_non_singleton)
+
+    def test_add_small_non_sing_shaped_int(self):
+        self.generic_various_shapes_test(np.add, ccm.Tensor.__add__, init2=lambda x: ccm.Tensor(x.tolist()), shapes=small_shapes_non_singleton)
+
+    def test_matmul_big_shaped_int(self):
+        self.generic_various_shapes_test(np.matmul, ccm.Tensor.__matmul__, init2=lambda x: ccm.Tensor(x.tolist()), shapes=big_shapes)
+
+    def test_add_big_shaped_int(self):
+        self.generic_various_shapes_test(np.add, ccm.Tensor.__add__, init2=lambda x: ccm.Tensor(x.tolist()), shapes=big_shapes)
+
+    def test_matmul_big_non_sing_shaped_int(self):
+        self.generic_various_shapes_test(np.matmul, ccm.Tensor.__matmul__, init2=lambda x: ccm.Tensor(x.tolist()),
+                                         shapes=big_shapes_non_singleton)
+
+    def test_add_big_non_sing_shaped_int(self):
+        self.generic_various_shapes_test(np.add, ccm.Tensor.__add__, init2=lambda x: ccm.Tensor(x.tolist()),
+                                         shapes=big_shapes_non_singleton)
